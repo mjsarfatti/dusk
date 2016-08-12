ba4ed19cf7ab3b94a0d80d8e4bcc0974b429f505
Manuele Sarfatti <mjsarfatti@gmail.com>
2016-08-13 00:44:51 +0200

    simplify, fix comments and make it compatible with IE

--- dist/dusk.js
+++ dist/dusk.js
@@ -1,3 +1,11 @@
+/*!
+ * dusk v0.0.1
+ * A feathery, alacritous DOM abstraction, partly based on PlainJS
+ * https://github.com/mjsarfatti/dusk#readme
+ * MIT License
+ * by Manuele J Sarfatti <mjsarfatti@gmail.com>
+ */
+
 (function (global, factory) {
   if (typeof define === "function" && define.amd) {
     define(['module', 'exports'], factory);
@@ -25,126 +33,122 @@
 
   /* global window, document, NodeList, HTMLCollection */
 
-  var dusk = function iife() {
+  /**
+   * Fastest way to turn a nodeList into a proper Array
+   * NOTE: it places nodeList at the beginning of the array!
+   * @param  {Array} array    Passed by reference
+   * @param  {nodeList|HTMLCollection}
+   * @return {void}           It just pushes them into the array
+   */
+  function pushInto(array, nodeList) {
+    var i = nodeList.length;
+    for (i; i--; array.unshift(nodeList[i])) {}
+  }
 
-    /**
-     * Fastest way to turn a nodeList into a proper Array
-     * NOTE: it places nodeList at the beginning of the array!
-     * @param  {Array} array    Passed by reference
-     * @param  {nodeList|HTMLCollection}
-     * @return {void}          It just pushes them into the array
-     */
-    function pushInto(array, nodeList) {
-      var i = nodeList.length;
-      for (i; i--; array.unshift(nodeList[i])) {}
-    }
+  // Us!
+  var dusk = function dusk(selector, context) {
+    // Select yo'self
+    if (selector && selector.dusked) return selector;
+    // Or return a new instance
+    // eslint-disable-next-line no-use-before-define
+    return new Dusker(selector, context);
+  };
 
-    var select = function select(selector, context) {
-      if (selector && selector.dusked) {
-        return selector;
-      }
+  dusk.fn = [];
+  dusk.fn.dusked = 1;
 
-      // The *jQuery* object is actually just the init constructor 'enhanced'
-      // eslint-disable-next-line new-cap
-      return new select.fn.init(selector, context);
-    };
+  var Dusker = function Dusker(selector, context) {
 
-    select.fn = [];
-    select.fn.dusked = 1;
+    var classes = void 0;
+    var els = void 0;
+    var realContext = void 0;
 
-    select.fn.init = function (selector, context) {
+    // If selector is string, do your best to use the built-ins and return ASAP
+    if (typeof selector === 'string') {
 
-      var classes = void 0;
-      var els = void 0;
-      var realContext = void 0;
+      // Return if context was not found
+      realContext = !context || context === document ? document : dusk(context)[0];
+      if (realContext === undefined) return this;
 
-      // If selector is string, do your job and return
-      if (typeof selector === 'string') {
-
-        // Return if context was not found
-        realContext = !context || context === document ? document : select(context)[0];
-        if (realContext === undefined) return this;
-
-        // Redirect simple selectors to the more performant function
-        if (/^(#?[\w-]+|\.[\w-.]+)$/.test(selector)) {
-          switch (selector.charAt(0)) {
-            case '#':
-              // Handle ID-based selectors
-              els = document.getElementById(selector.substr(1));
-              if (els !== null) {
-                this[0] = els;
-                this.length = 1;
-              }
-              return this;
-            case '.':
-              // Handle class-based selectors
-              // Query by multiple classes by converting the selector
-              // string into single spaced class names
-              classes = selector.substr(1).replace(/\./g, ' ');
-              els = realContext.getElementsByClassName(classes);
-              pushInto(this, els);
-              return this;
-            default:
-              // Handle tag-based selectors
-              els = realContext.getElementsByTagName(selector);
-              pushInto(this, els);
-              return this;
-          }
-          // Handles empty string
-        } else if (selector !== '') {
-          els = realContext.querySelectorAll(selector);
-          pushInto(this, els);
-          return this;
-        } else {
-          return this;
+      // Redirect simple selectors to the most performant function
+      if (/^(#?[\w-]+|\.[\w-.]+)$/.test(selector)) {
+        switch (selector.charAt(0)) {
+          case '#':
+            // Handle ID-based selectors
+            els = document.getElementById(selector.substr(1));
+            if (els !== null) {
+              this[0] = els;
+              this.length = 1;
+            }
+            return this;
+          case '.':
+            // Handle class-based selectors
+            // Query by multiple classes by converting the selector
+            // string into single spaced class names
+            classes = selector.substr(1).replace(/\./g, ' ');
+            els = realContext.getElementsByClassName(classes);
+            pushInto(this, els);
+            return this;
+          default:
+            // Handle tag-based selectors
+            els = realContext.getElementsByTagName(selector);
+            pushInto(this, els);
+            return this;
         }
-      } else if ((typeof selector === 'undefined' ? 'undefined' : _typeof(selector)) === 'object') {
-
-        // Handle a node (and the null object)
-        if (selector && selector.addEventListener) {
-          this.push(selector);
-          // Handle a list of nodes
-        } else if ({}.isPrototypeOf.call(NodeList.prototype, selector) || {}.isPrototypeOf.call(HTMLCollection.prototype, selector)) {
-          // eslint-disable-next-line prefer-spread
-          this.push.apply(this, selector);
-        }
-
-        // Return early
+        // Handle empty string
+      } else if (selector !== '') {
+        els = realContext.querySelectorAll(selector);
+        pushInto(this, els);
         return this;
       }
 
-      // Default (handles undefined and false)
       return this;
-    };
+    } else if ((typeof selector === 'undefined' ? 'undefined' : _typeof(selector)) === 'object') {
 
-    // Give the init function the *jQuery* prototype for later instantiation
-    select.fn.init.prototype = select.fn;
+      // Handle a node (and the null object)
+      if (selector && selector.addEventListener) {
+        this.push(selector);
 
-    // DOMReady static method (stolen from jQuery)
-    select.DOMReady = function DOMReady(ready) {
+        // Handle a list of nodes
+      } else if (NodeList.prototype.isPrototypeOf(selector) // eslint-disable-line
+      || HTMLCollection.prototype.isPrototypeOf(selector) // eslint-disable-line
+      ) {
+          pushInto(this, selector);
+        }
 
-      // The ready event handler and self cleanup method
-      function completed() {
-        document.removeEventListener('DOMContentLoaded', completed);
-        window.removeEventListener('load', completed);
-        ready();
-      }
+      return this;
+    }
 
-      // Older IE sometimes signals "interactive" too soon
-      if (document.readyState === 'complete' || document.readyState !== 'loading' && !document.documentElement.doScroll) {
-        // Handle it asynchronously to allow scripts the opportunity to delay ready
-        window.setTimeout(ready);
-      } else {
-        // Use the handy event callback
-        document.addEventListener('DOMContentLoaded', completed);
-        // A fallback to window.onload, that will always work
-        window.addEventListener('load', completed);
-      }
-    };
+    // Default (handles undefined and false)
+    return this;
+  };
 
-    return select;
-  }();
+  // Give the init function the dusk prototype for later instantiation
+  Dusker.prototype = dusk.fn;
+
+  // DOMReady static method (shamelessly stolen from jQuery)
+  dusk.DOMReady = function DOMReady(ready) {
+
+    // The ready event handler and self cleanup method
+    function completed() {
+      document.removeEventListener('DOMContentLoaded', completed);
+      window.removeEventListener('load', completed);
+      ready();
+    }
+
+    // Older IE sometimes signals "interactive" too soon
+    if (document.readyState === 'complete' || document.readyState !== 'loading' && !document.documentElement.doScroll) {
+      // Handle it asynchronously to allow scripts the opportunity to delay ready
+      window.setTimeout(ready);
+    } else {
+      // Use the handy event callback
+      document.addEventListener('DOMContentLoaded', completed);
+      // A fallback to window.onload, that will always work
+      window.addEventListener('load', completed);
+    }
+  };
 
   exports.default = dusk;
   module.exports = exports['default'];
 });
+
